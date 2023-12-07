6808f883f94bda16d607fe680b568ecd600f34af
Cai0Aqu1no <152229338+Cai0Aqu1no@users.noreply.github.com>
2023-12-07 20:27:37 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        let retorno = confirm('Confirma a devolução?');
+        if (retorno == true) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
+        else {
+            return;
+        }
+        
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
